45dda543afd164991ee87d760d72d43de9c1013f
Lautaro <lautarorisso77@gmail.com>
2026-03-09 13:46:45 -0300

    feat: implement PostCOntroller with CRUD endpoints

---
+++ src/main/java/com/lautarorisso/blogging_platform_api/controller/PostController.java
@@ -0,0 +1,49 @@
+package com.lautarorisso.blogging_platform_api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lautarorisso.blogging_platform_api.model.Post;
+import com.lautarorisso.blogging_platform_api.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postService.getPostById(id);
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
+        return postService.updatePost(id, updatedPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+    }
+}
